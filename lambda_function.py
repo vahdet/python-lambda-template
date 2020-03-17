3ff2a3fc4ee14b137ae661d68b63fb56d9c00d64
100117 <vahdet.keskin@isbank.com.tr>
2020-03-17 11:25:09 +0300

    add api key with boto3

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,17 @@
+""" Lambda function module """
+import os
 import json
+import boto3
 
 def lambda_handler(event, context):
-    # TODO implement
+    """ Handles function event and context """
+
+    # resolve backend api key from the secrets manager
+    sm_client = boto3.client('secretsmanager')
+    sm_resp = sm_client.get_secret_value(os.getenv('BACKEND_SERVICE_API_KEY_SECRET_ARN'))
+    backend_api_key = json.dumps(sm_resp.get('SecretString')).get('key')
+
+    # TODO implement further business logic
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
